8305de39cfab782e329920c70c02b4295a755846
Gabriel-Wichtendahl <118149051+Gabriel-Wichtendahl@users.noreply.github.com>
2026-08-31 09:16:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-/* sw.js — Deriv Inicio Inamovible v113.33-II54 */
+/* sw.js — Deriv Inicio Inamovible v113.33-II55 */
 "use strict";
 
 // Caché exclusiva de Inicio Inamovible para que no colisione con la PWA estable ni con II1.
-const CACHE = "deriv-assets-v113-33-inicio-inamovible-ii54";
+const CACHE = "deriv-assets-v113-33-inicio-inamovible-ii55";
 
 const CORE_ASSETS = [
   "./",
